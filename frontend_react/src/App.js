565363d01400f729cb53cd801b2078935932ebab
stephen <nwabuezesc@gmail.com>
2022-06-19 19:10:26 +0100

    new data

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom'
 import {Home,WeddingStories, WeddingStoriesDetails } from './container'
 import { Navbar} from './miniComponent'
+import BarLoader from "react-spinners/BarLoader";
 import './App.css'
 
+
 function App() {
+  const [ loading, setLoading ] = useState(false)
+
+  useEffect(() =>{
+    setLoading(true)
+    setTimeout(() => (
+      setLoading(false)
+    ), 2000)
+  }, [])
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home /> }/>
-        <Route path="/weddingstories" element={<WeddingStories /> }/>
-        <Route path="/weddingstories/:id" element={<WeddingStoriesDetails /> }/>
-      </Routes>
+      {
+        loading ? 
+        <div className='loader'><BarLoader  color={'#fff'} loading={loading} size={150} /></div>
+        :
+        <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home /> }/>
+          <Route path="/weddingstories" element={<WeddingStories /> }/>
+          <Route path="/weddingstories/:id" element={<WeddingStoriesDetails /> }/>
+        </Routes>
+        </div>
+      }
     </div>
   );
 }
